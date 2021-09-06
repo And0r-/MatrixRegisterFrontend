bc1447e657e5e6833ee49c801b6b960dab8f1b17
DESKTOP-E98Q6N0\And0r <ahabegger@gmail.com>
2021-09-06 17:47:32 +0200

    remove old routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import CreateUserDialog from './Form';
-import RegisterDone from './RegisterDone';
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,15 +12,7 @@ function App() {
       {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
       <Switch>
-        <Route path="/register/:token" component={CreateUserDialog}>
-          
-        </Route>
-        <Route path="/done">
-          <RegisterDone />
-        </Route>
-        <Route path="/">
-          
-        </Route>
+        <Route path="/register/:token" component={CreateUserDialog} />
       </Switch>
     </Router>
 

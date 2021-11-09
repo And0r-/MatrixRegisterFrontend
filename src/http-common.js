68b47c5ca7549bdceaefe12a9d9c932e7dbd6473
DESKTOP-E98Q6N0\And0r <ahabegger@gmail.com>
2021-11-09 04:17:25 +0100

    use current token

--- src/http-common.js
+++ src/http-common.js
@@ -1,14 +1,25 @@
 import axios from "axios";
 import stage_config from './config';
 
-const token = window.accessToken ? window.accessToken : 'dummy_token';
 
-export default axios.create({
-  baseURL: stage_config.apiGateway.URL,
-  headers: {
-    "Content-type": "application/json",
-    "Authorization": 'Bearer ' + token
-  }
+const axiosInstance = axios.create({
+    baseURL: stage_config.apiGateway.URL
 });
 
+axiosInstance.interceptors.request.use(
+    config => {
+        const token = window.accessToken ? window.accessToken : 'dummy_token';
+        config.headers['Authorization'] = 'Bearer ' + token;
+        return config;
+    },
+    error => {
+        Promise.reject(error)
+    });
 
+axiosInstance.interceptors.response.use((response) => {
+    return response
+}, function (error) {
+    return Promise.reject(error);
+});
+
+export default axiosInstance;

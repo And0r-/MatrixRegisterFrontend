ceeb3e8e51c9d36897ee36f989eb9b9f2a147154
DESKTOP-E98Q6N0\And0r <ahabegger@gmail.com>
2021-12-13 03:48:45 +0100

    remove Matrix access management, I have now a new project that will do it as a cron. One click is one click to much

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import CreateUserDialog from './Form';
 import PrivateRoute from './PrivateRoute';
-import GetMatrixToken from './GetMatrixToken';
-import SetMatrixAccess from './SetMatrixAccess';
 import Projects from './Projects';
 import {
   BrowserRouter as Router,
@@ -17,8 +15,6 @@ function App() {
         <Route exact  path="/register/:token" component={CreateUserDialog} />
 
         <PrivateRoute exact  path="/" component={Projects} />
-        <Route exact  path="/getMatrixToken" component={GetMatrixToken} />
-        <PrivateRoute exact  path="/setMatrixAccess" component={SetMatrixAccess} />
 
       </Switch>
     </Router>
